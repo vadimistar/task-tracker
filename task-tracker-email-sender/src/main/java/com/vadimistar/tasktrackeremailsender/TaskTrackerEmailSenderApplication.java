873f8ab1b3d673c955925b964d133ef5bd83ec50
vadimistar <vadimistar@gmail.com>
2024-09-11 20:28:49 +0300

    Create all applications

---
+++ task-tracker-email-sender/src/main/java/com/vadimistar/tasktrackeremailsender/TaskTrackerEmailSenderApplication.java
@@ -0,0 +1,13 @@
+package com.vadimistar.tasktrackeremailsender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskTrackerEmailSenderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskTrackerEmailSenderApplication.class, args);
+	}
+
+}
---
+++ task-tracker-scheduler/src/test/java/com/vadimistar/tasktrackerscheduler/TaskTrackerSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vadimistar.tasktrackerscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskTrackerSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
